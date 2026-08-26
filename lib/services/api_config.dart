726fb4283888ea9a37ecf9be180743e550fddc12
AbdullayevHayotjon <hayot3003@gmail.com>
2026-08-26 14:23:38 +0500

    mobile ilova ham realniy bazaga boglandi

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,6 +1,6 @@
 const apiBaseUrl = String.fromEnvironment(
   'API_BASE_URL',
-  defaultValue: 'http://172.29.90.122:8080/api',
+  defaultValue: 'https://api.goarena.uz/api',
 );
 
 const androidStoreUrl = String.fromEnvironment(
